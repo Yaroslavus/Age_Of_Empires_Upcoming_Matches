571719e28ce3b769a478c15edd013df6d22d3525
yaroslav_sagan <yaroslav.sagan@gmail.com>
2023-04-04 16:40:31 +0400

    Correct local time, correct timedeltas to the start of the game
    But the strings format is wrong yet

--- AOE_2 Live Timetable.py
+++ AOE_2 Live Timetable.py
@@ -15,7 +15,7 @@ class UpcomingMatchesViewer(tk.LabelFrame):
         self.grid_columnconfigure(1, weight=1)
         refresh_checkbox = tk.Checkbutton(self, text="Put on the flag if you really want to make a full refresh", onvalue=True, offvalue=False)
         refresh_checkbox.grid(row=0, column=0, sticky="nsew")
-        refresh_button = tk.Button(self, text="Refresh all Information (takes about 30 sec)", command=lambda: self.full_refresh())
+        refresh_button = tk.Button(self, text="Refresh all Information (takes about 30 sec)")
         refresh_button.grid(row=0, column=1, sticky="w")
         tk.Label(self, text="Upcoming Tournaments:", anchor="w").grid(row=1, column=0, sticky="ew")
         tk.Label(self).grid(row=2, column=0, sticky="ew", columnspan=4)
@@ -46,14 +46,15 @@ class UpcomingMatchesViewer(tk.LabelFrame):
 
     def full_refresh(self):
         if askyesno(title="Full Refreshing", message="Do you really want to refresh the form?"):
-            refresh_label = tk.Label(self, text="Refreshing...", anchor="w")
-            refresh_label.grid(row=0, column=2, sticky="w")
-            time.sleep(2)
-            UpcomingMatchesViewer(root).pack(side="top", fill="both", expand=True, padx=10, pady=10)
-            refresh_label = tk.Label(self, text="Refreshed", anchor="w")
-            refresh_label.grid(row=0, column=2, sticky="w")
-            time.sleep(0.5)
-            tk.Label(self).grid(row=2, column=0, sticky="ew", columnspan=4)
+            self.update()
+#            refresh_label = tk.Label(self, text="Refreshing...", anchor="w")
+#            refresh_label.grid(row=0, column=2, sticky="w")
+#            time.sleep(2)
+#            UpcomingMatchesViewer(root).pack(side="top", fill="both", expand=True, padx=10, pady=10)
+#            refresh_label = tk.Label(self, text="Refreshed", anchor="w")
+#            refresh_label.grid(row=0, column=2, sticky="w")
+#            time.sleep(0.5)
+#            tk.Label(self).grid(row=2, column=0, sticky="ew", columnspan=4)
         else:
             pass
 
@@ -78,10 +79,22 @@ class UpcomingMatchesViewer(tk.LabelFrame):
                 tournament_tier.grid(row=self.row + 1, column=0, sticky="ew")
                 tournament_date.grid(row=self.row + 2, column=0, sticky="ew")
                 for game in games_list:
-                    if self.__activity_status(game, 1) in ["LIVE!", "Soon"]:
+                    start_time_local = game.start_datetime_local.strftime("%d.%m.%y %H:%M")
+                    if self.__activity_status(game, 1)  == "LIVE!":
                         game_title = tk.Label(self, text=f'{game.team_1} vs. {game.team_2}', anchor="w")
-                        game_time = tk.Label(self, text=f'{game.time}', anchor="w")
-                        game_activity_status = tk.Label(self, text=f'{self.__activity_status(game, 1)}', anchor="w")
+                        game_time = tk.Label(self, text=f'{start_time_local}', anchor="w")
+                        game_activity_status = tk.Label(self, text="LIVE!", anchor="w")
+                        game_stage = tk.Label(self, text=f'{game.stage}', anchor="w")
+                        game_title.grid(row=self.row, column=1, sticky="ew")
+                        game_time.grid(row=self.row, column=2, sticky="ew")
+                        game_activity_status.grid(row=self.row, column=3, sticky="ew")
+                        game_stage.grid(row=self.row, column=4, sticky="ew")
+                        self.row += 1
+                    elif self.__activity_status(game, 1)  == "Soon":
+                        timedelta = game.start_datetime_local - datetime.now()
+                        game_title = tk.Label(self, text=f'{game.team_1} vs. {game.team_2}', anchor="w")
+                        game_time = tk.Label(self, text=f'{start_time_local}', anchor="w")
+                        game_activity_status = tk.Label(self, text=f'{timedelta}', anchor="w")
                         game_stage = tk.Label(self, text=f'{game.stage}', anchor="w")
                         game_title.grid(row=self.row, column=1, sticky="ew")
                         game_time.grid(row=self.row, column=2, sticky="ew")
